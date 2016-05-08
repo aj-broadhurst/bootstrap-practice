3d8e406d8f00cb6ad82583a83fde79e2a861eda3
Alicia Broadhurst <abroadhurst@cnm.edu>
2016-05-08 14:55:00 -0600

    done with assignment

--- index.php
+++ index.php
@@ -4,37 +4,179 @@
 		<meta charset="UTF-8">
 
 		<!-- sets IE rendering to IE Edge -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 
 		<!-- sets viewport width to device width, scaling 1:1  -->
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+				integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
 		<!-- Optional theme --> <!-- Basically borders, gradients, etc -->
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous" />
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+				integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r"
+				crossorigin="anonymous"/>
+
+
+		<!-- Font Awesome Icons -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
 
 		<!-- Custom CSS -->
 		<link rel="stylesheet" href="style.css" type="text/css">
 
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="http://code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
+		<script src="http://code.jquery.com/jquery-2.2.3.min.js"
+				  integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+				  integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+				  crossorigin="anonymous"></script>
 
 		<!-- Add own Javascript here -->
 
 		<title>Bootstrap Practice</title>
 	</head>
 
-	<body>
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12"> 12/12 on all screen sizes</div>
-			</div>
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<header>
+				<div class="container">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+										  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+								<a class="navbar-brand" href="https://bootcamp-coders.cnm.edu/~abroadhurst/bootstrap-practice/">
+									Bootstrap Practice</a>
+							</div>
+							<!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+								<ul class="nav navbar-nav navbar-right">
+									<li><a href="http://getbootstrap.com/">Bootstrap</a></li>
+									<li><a href="https://bootcamp-coders.cnm.edu/class-materials/bootstrap/">DDCB Bootstrap
+											Intro</a></li>
+									<li><a href="https://google.com">Google</a></li>
+								</ul>
+							</div><!-- /.navbar-collapse -->
+						</div>
+				</div>
+
+			</header>
+			<main>
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="well text-center">
+								<h1>This is Bootstrap Practice!</h1>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							<div class="well">
+								<h3>Column 1</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin condimentum lectus vitae
+									ultricies malesuada. Phasellus sit amet feugiat magna. Phasellus. </p>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="well">
+								<h3>Column 2</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin condimentum lectus vitae
+									ultricies malesuada. Phasellus sit amet feugiat magna. Phasellus. </p>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="well">
+								<h3>Column 3</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin condimentum lectus vitae
+									ultricies malesuada. Phasellus sit amet feugiat magna. Phasellus. </p>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="well">
+								<h3>Column 4</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin condimentum lectus vitae
+									ultricies malesuada. Phasellus sit amet feugiat magna. Phasellus.</p>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6">
+							<div class="well">
+								<h4>Section 1</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu ante, pretium non nisi
+									ut, maximus accumsan ipsum. Aliquam dolor nulla, cursus ut augue eu, ornare pretium ligula.
+									Morbi in porta enim, eu egestas lectus. Pellentesque vitae nisi eros. Cum sociis natoque
+									penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut sagittis urna in ornare
+									mollis. Nunc maximus ac nibh a blandit. Mauris dignissim turpis nunc, quis varius metus
+									malesuada a. Suspendisse et justo mattis, iaculis lacus id, sollicitudin libero. Phasellus
+									purus lacus, viverra at leo vitae, sodales sodales arcu. Nulla at justo a massa accumsan
+									convallis. Donec porta urna id ante varius ornare. Vivamus pellentesque enim vel lorem
+									finibus gravida. Cras dui eros, tempor non fermentum in, pulvinar in lorem. Aliquam id
+									fermentum metus. </p>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="well">
+								<h4>Section 2</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris arcu ante, pretium non nisi
+									ut, maximus accumsan ipsum. Aliquam dolor nulla, cursus ut augue eu, ornare pretium ligula.
+									Morbi in porta enim, eu egestas lectus. Pellentesque vitae nisi eros. Cum sociis natoque
+									penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut sagittis urna in ornare
+									mollis. Nunc maximus ac nibh a blandit. Mauris dignissim turpis nunc, quis varius metus
+									malesuada a. Suspendisse et justo mattis, iaculis lacus id, sollicitudin libero. Phasellus
+									purus lacus, viverra at leo vitae, sodales sodales arcu. Nulla at justo a massa accumsan
+									convallis. Donec porta urna id ante varius ornare. Vivamus pellentesque enim vel lorem
+									finibus gravida. Cras dui eros, tempor non fermentum in, pulvinar in lorem. Aliquam id
+									fermentum metus.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
 		</div>
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="foot-content">
+							<div class="well">
+								<p>Text here</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="foot-content">
+							<div class="well">
+								<p>More text here</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="bottom-center">
+							<p>Centered text</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</footer>
 	</body>
 
 </html>
